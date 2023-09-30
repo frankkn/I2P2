00d642a83a34981735e4b7a29a4ee17d98ccc39f
frankkn <frank840629@gmail.com>
2023-09-30 12:15:03 +0800

    update

---
+++ homework1/Typing/0930.c
@@ -0,0 +1,64 @@
+#define next ptr_to_next_node
+#define prev ptr_to_prev_node
+
+void insert(Node **C, char c)
+{
+    Node* cur = (*C);
+    Node* b=(Node*)malloc(sizeof(Node));
+    b->character = c;
+    Node* a = cur->next;
+    a->prev = b;
+    b->next = a;
+    cur->next = b;
+    b->prev = cur;
+}
+
+void deletion(Node **C)
+{
+    Node* cur = (*C);
+    if(cur->next == head)	return;
+    Node* b = cur->next;
+	Node* a = b->next;
+    cur->next = a;
+    a->prev = cur;
+    free(b);
+}
+
+void backspace(Node **C)
+{
+    
+    if((*C) == head) return;
+    (*C) = (*C)->prev;
+    deletion(C);
+
+    /* [BUG!]
+    Node* cur = (*C);
+    if(cur == head)	return;
+    cur = cur->prev;
+    deletion(&cur);
+    */
+}
+
+void go_left(Node **cur, int t)
+{
+    for(int i = 0; i < t; ++i){
+    	(*cur) = (*cur)->prev;
+    }
+}
+
+void go_right(Node **cur, int t)
+{
+    for(int i = 0; i < t; ++i){
+    	(*cur) = (*cur)->next;
+    }
+}
+
+void go_home(Node **cur)
+{
+    (*cur) = head;
+}
+
+void go_end(Node **cur)
+{
+    (*cur) = head->prev;
+}

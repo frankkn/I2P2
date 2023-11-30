93e347cb00d838b37a693c6e71193e861bcf41d8
frankkn <frank840629@gmail.com>
2023-12-01 00:17:14 +0800

    update

--- 3_access_specifiers/a.cpp
+++ 3_access_specifiers/a.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 class D{
     public:
-        void out_protect() { cout << _d << "\n"; }
-        void out_private() { cout << __d << "\n"; }
+        void out_protect() { cout << typeid(*this).name() << "," << _d << "\n"; }
+        void out_private() { cout << typeid(*this).name() << "," << __d << "\n"; }
         int d;
     protected:
         int _d;
@@ -15,7 +16,7 @@ D object_D;
 
 class B: public D{
     public:
-        void out_protect_B() { cout << _d << "\n"; }
+        void out_protect_B() { cout << typeid(*this).name() << "," << _d << "\n"; }
 };
 B object_B;
 
@@ -30,6 +31,6 @@ int main(){
     // cout << object_B.__d; // X
     object_B.out_protect(); // O
     object_B.out_protect_B(); // O
-    object_B.out_private(); // X
+    object_B.out_private(); // O
     return 0;
 }
--- 3_access_specifiers/b.cpp
+++ 3_access_specifiers/b.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 class D{
     public:
-        void out_protect() { cout << _d << "\n"; }
-        void out_private() { cout << __d << "\n"; }
+        void out_protect() { cout << typeid(*this).name() << "," << _d << "\n"; }
+        void out_private() { 
+            cout << typeid(*this).name() << "," << __d << "\n"; 
+        }
         int d;
     protected:
         int _d;
@@ -16,16 +19,16 @@ D object_D;
 // protected 繼承D
 class B: protected D{
     public:
-        void out_protect_B() { cout << _d << "\n"; }
+        void out_protect_B() { cout << typeid(*this).name() << "," << _d << "\n"; }
 };
 B object_B;
 
 int main(){
-    cout << object_B.d << "\n"; // O
+    // cout << object_B.d << "\n"; // X 由於類別 B 使用了 protected 繼承 D，object_B.d 變成了 protected 成員，因此在 main 函數中無法直接訪問。
     // cout << object_B._d << "\n"; // X
     // cout << object_B.__d << "\n"; // X
-    object_B.out_protect(); // X, out_protect() 變成 protected
+    // object_B.out_protect(); // X, out_protect() 變成 protected
     object_B.out_protect_B(); // O
-    object_B.out_private(); // X
+    // object_B.out_private(); // X 由於類別 B 使用了 protected 繼承 D，out_private() 變成了 protected 成員，因此在 main 函數中無法直接訪問。
     return 0;
 }
